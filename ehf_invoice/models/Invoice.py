6047f977c0344b019df4772c7b0ddc4e5851ed4c
datagutten <datagutten@datagutten.net>
2021-01-25 10:08:40 +0100

    Invoice number is only unique for each supplier

--- ehf_invoice/models/Invoice.py
+++ ehf_invoice/models/Invoice.py
@@ -10,10 +10,13 @@ class Invoice(models.Model):
     customer = models.ForeignKey(
         Customer, on_delete=models.PROTECT, verbose_name='kunde'
     )
-    invoice_number = models.CharField(primary_key=True, max_length=100)
+    invoice_number = models.CharField(max_length=100)
     order_number = models.CharField(max_length=100, blank=True, null=True)
     date = models.DateField()
-    amount = models.DecimalField(decimal_places=2, max_digits=8)
+    amount = models.DecimalField(decimal_places=2, max_digits=12)
+
+    class Meta:
+        unique_together = ['supplier', 'invoice_number']
 
     def __str__(self):
         return '%s invoice %s' % (self.supplier, self.invoice_number)
