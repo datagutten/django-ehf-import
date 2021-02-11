0e024aaad40f7adee4b0f1cf870852795578b706
datagutten <datagutten@datagutten.net>
2021-02-11 20:23:31 +0100

    Add unique file name field

--- ehf_invoice/models/Attachment.py
+++ ehf_invoice/models/Attachment.py
@@ -7,10 +7,11 @@ class Attachment(models.Model):
     invoice = models.ForeignKey(
         Invoice, on_delete=models.CASCADE, related_name='attachments'
     )
+    name = models.CharField(max_length=255)
     file = models.FileField(upload_to='invoice_attachments/%Y/')
     mime = models.CharField('MIME type', max_length=50)
 
     class Meta:
-        unique_together = ['invoice', 'file']
+        unique_together = ['invoice', 'name']
         verbose_name = 'vedlegg'
         verbose_name_plural = 'vedlegg'
