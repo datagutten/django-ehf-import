d8443e11317fceb690e20d7eb5a484768342baf2
datagutten <datagutten@datagutten.net>
2021-02-12 10:18:15 +0100

    Require permission

--- ehf_invoice/views.py
+++ ehf_invoice/views.py
@@ -1,13 +1,16 @@
+from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render
 
 from ehf_invoice.models import Invoice, SerialNumber
 
 
+@permission_required('ehf_invoice.view_invoice', raise_exception=True)
 def show_invoice(request, invoice_number):
     invoice = Invoice.objects.get(invoice_number=invoice_number)
     return render(request, 'ehf_invoice/invoice.html', {'invoice': invoice})
 
 
+@permission_required('ehf_invoice.view_serialnumber', raise_exception=True)
 def find_serial(request, serial_number):
     serial = SerialNumber.objects.get(serial=serial_number)
     invoice = serial.item.invoice
