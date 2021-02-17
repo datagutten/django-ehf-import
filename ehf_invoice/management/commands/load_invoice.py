8bce27ed3a653284aba196b49fe94980b04dc5f8
datagutten <datagutten@datagutten.net>
2021-02-17 18:54:42 +0100

    Check if directory entry is file

--- ehf_invoice/management/commands/load_invoice.py
+++ ehf_invoice/management/commands/load_invoice.py
@@ -88,4 +88,5 @@ class Command(BaseCommand):
             return self.load(options['file'][0])
         elif os.path.isdir(options['file'][0]):
             for file in os.scandir(options['file'][0]):
-                self.load(file)
+                if os.path.isfile(file):
+                    self.load(file)
