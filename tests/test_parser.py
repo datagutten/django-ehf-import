94543e99d1dc1edc13d4d412d910ffc7ff90bf7a
datagutten <datagutten@datagutten.net>
2021-04-22 11:45:24 +0200

    Test line quantity

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -15,6 +15,8 @@ class ParserTestCase(unittest.TestCase):
         self.assertEqual('2013-06-30', invoice.invoice_date)
         self.assertEqual('TOSL108', invoice.invoice_number)
         self.assertEqual('Contract321', invoice.order_number)
+        line = invoice.invoice_lines()[0]
+        self.assertEqual(line.quantity, 1)
 
     def test_credit(self):
         invoice = InvoiceXML(
@@ -22,6 +24,8 @@ class ParserTestCase(unittest.TestCase):
                          'Kreditnota.xml'))
         self.assertEqual(-1436.5, invoice.amount)
         self.assertEqual(5, len(invoice.invoice_lines()))
+        line = invoice.invoice_lines()[0]
+        self.assertEqual(line.quantity, 1)
 
 
 if __name__ == '__main__':
