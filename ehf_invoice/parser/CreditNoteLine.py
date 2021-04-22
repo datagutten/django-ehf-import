cb648d6840406565047b4a50a958d8ded0b399d9
datagutten <datagutten@datagutten.net>
2021-04-22 14:56:52 +0200

    Negate all amounts for credit notes

--- ehf_invoice/parser/CreditNoteLine.py
+++ ehf_invoice/parser/CreditNoteLine.py
@@ -5,4 +5,12 @@ class CreditNoteLine(InvoiceLine):
     @property
     def quantity(self):
         q = self.line.find('cbc:CreditedQuantity', self.namespaces).text
-        return int(float(q))
+        return -int(float(q))
+
+    @property
+    def price(self):
+        return -super(CreditNoteLine, self).price
+
+    @property
+    def sum(self):
+        return -super(CreditNoteLine, self).sum
