7f2c06b56948854b00e16a2696472c9d996c9368
datagutten <datagutten@datagutten.net>
2021-12-02 12:12:13 +0100

    Add alternate id to Supplier

--- ehf_invoice/models/Supplier.py
+++ ehf_invoice/models/Supplier.py
@@ -3,6 +3,8 @@ from django.db import models
 
 class Supplier(models.Model):
     id = models.IntegerField('organisasjonsnummer', primary_key=True)
+    alternate_id = models.CharField('organisasjonsnummer (ikke numerisk)', unique=True, null=True, blank=True,
+                                    max_length=50)
     name = models.CharField('navn', max_length=200)
 
     class Meta:
