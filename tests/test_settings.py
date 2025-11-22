e8526284929df8ce86bad31e48eb34bb59799bfb
Anders Birkenes <datagutten@datagutten.net>
2025-11-22 18:12:39 +0100

    Support Django 5

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -102,3 +102,8 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 # STATIC_URL = '/static/'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
